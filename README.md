b84ffa48643e3ca77d0df3dcce26bdf028c298ab
Alix von Schirp <github@avonschirp.bootmedia.de>
2024-12-09 06:35:03 +0100

    Basics, Setup, Launcher

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# aoc2024
+# Advent of Coding 2024
+
+<div align="center">
+
+[![Project license](https://img.shields.io/github/license/b00tload/advent-of-coding-2024.svg?style=flat-square)](LICENSE)
+![GitHub last commit](https://img.shields.io/github/last-commit/b00tload/advent-of-coding-2024?style=flat-square)
+
+
+</div>
+
+<details open="open">
+<summary>Table of Contents</summary>
+
+- [About](#about)
+    - [Built With](#built-with)
+- [Getting Started](#getting-started)
+    - [Prerequisites](#prerequisites)
+    - [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Acknowledgements](#acknowledgements)
+
+</details>
+
+---
+
+## About
+
+My solutions to the Advent of Code 2024.
+
+### Built With
+
+- TypeScript
+- Launcher build with inquirer.js
+
+## Getting Started
+
+### Prerequisites
+
+- pnpm 9
+- node 20
+
+### Installation
+
+1. git clone
+2. pnpm install
+3. pnpm start
+
+## Usage
+
+- run `pnpm start`
+- download your input
+- Select day
+- select part
+- select input
+
+## License
+
+This project is licensed under the **ISC license**.
+
+See [LICENSE](LICENSE) for more information.
+
+## Acknowledgements
+
+Thanks to [Eric Wastl](https://was.tl/) for creating Advent of Code!
---
+++ index.ts
@@ -0,0 +1,119 @@
+import {search} from '@inquirer/prompts'
+import fs from "node:fs";
+import * as process from "node:process";
+import * as path from "node:path";
+
+
+type Choice<Value> = {
+  value: Value;
+  name?: string;
+  description?: string;
+  short?: string;
+  disabled?: boolean | string;
+};
+
+async function main(day: string | undefined, part : string | undefined, input: string | undefined) {
+  if ((day ?? null) == null) {
+    day = await search({
+      message: "Select a day",
+      source: async (input) => {
+        if (!input) return fs.readdirSync("./", {withFileTypes: true})
+          .filter(value => value.isDirectory())
+          .filter(value => {
+            return !value.name.match("node_modules")
+          }).filter(value => {
+            return !value.name.startsWith(".")
+          }).map(value => value.name)
+
+        return fs.readdirSync("./", {withFileTypes: true})
+          .filter(value => {
+            return (path.join(value.parentPath, value.name)).match(input)
+          })
+          .filter(value => value.isDirectory())
+          .filter(value => {
+            return !value.name.match("node_modules")
+          }).filter(value => {
+            return !value.name.startsWith(".")
+          }).map(value => value.name)
+      }
+    })
+  }
+
+  if ((part ?? null) == null) {
+    part = await search({
+      message: "Select a Part",
+      source: async (input) => {
+        if (!input) return fs.readdirSync(`./${day}`, {withFileTypes: true})
+          .filter(value => value.isDirectory())
+          .filter(value => {
+            return !value.name.match("node_modules")
+          }).filter(value => {
+            return !value.name.startsWith(".")
+          }).map(value => value.name)
+
+        return fs.readdirSync("./", {withFileTypes: true})
+          .filter(value => {
+            return (path.join(value.parentPath, value.name)).match(input)
+          })
+          .filter(value => value.isDirectory())
+          .filter(value => {
+            return !value.name.startsWith(".")
+          }).map(value => value.name)
+      }
+    })
+  }
+
+  if ((input ?? null) == null) {
+
+    const inputs = fs.readdirSync(`./${day}`, {withFileTypes: true})
+      .filter(value => value.isFile())
+      .filter(value => value.isFile()).filter(value => {
+        return !value.parentPath.match("node_modules")
+      }).filter(value => {
+        return !value.parentPath.startsWith(".") || value.parentPath.startsWith("./")
+      }).filter(value => {
+        return !value.name.startsWith(".")
+      }).map<Choice<string>>(value => {
+        return {
+          name: value.name,
+          value: path.join(value.parentPath, value.name)
+        }
+      })
+
+
+    input = await search({
+      message: `Select file (Select within ./${day} or search within ./*))`,
+      source: async (input) => {
+        if (!input) return inputs;
+
+        return fs.readdirSync("./", {recursive: true, withFileTypes: true})
+          .filter(value => {
+          return (path.join(value.parentPath, value.name)).match(input)
+        }).filter(value => value.isFile())
+          .filter(value => value.isFile()).filter(value => {
+          return !value.parentPath.match("node_modules")
+        }).filter(value => {
+          return !value.parentPath.startsWith(".") || value.parentPath.startsWith("./")
+        }).filter(value => {
+          return !value.name.startsWith(".")
+        }).map<Choice<string>>(value => {
+          return {
+            name: path.join(value.parentPath, value.name),
+            value: path.join(value.parentPath, value.name)
+          }
+        })
+      }
+    })
+  }
+
+  console.log(input)
+
+  const {default: runner} = await import(`./${day}/${part}/index.js`)
+  runner(input)
+}
+
+const day = process.argv[2]
+const part = process.argv[3]
+const input = process.argv[4]
+
+main(day, part, input).then()
